9b055dd89f54f9283ecfa2ead64f21b223338951
Hugo.V <hugo.vialaneix@gmail.com>
2025-09-21 22:54:30 +0200

    Foo's getter and setter

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -1,4 +1,37 @@
 package com.jad;
 
+import java.util.List;
+
 public class Foo {
+    private Bar bar;
+    private List<Baz> bazs;
+    private Qux qux;
+    private Corge corge;
+    private List<Grault> graults;
+
+    public List<Grault> getGraults() {
+        return this.graults;
+    }
+
+    public List<Baz> getBazs() {
+        return this.bazs;
+    }
+
+    public Corge getCorge() {
+        return this.corge;
+    }
+
+    public void setCorge(Corge corge) {
+        this.corge = corge;
+    }
+
+
+    public Bar getBar() {
+        return bar;
+
+    }
+
+    public Qux getQux() {
+        return qux;
+    }
 }
